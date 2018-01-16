7ce23a504f3b42b59afe03bdb3e1bdf4358bd864
Artem Parhomenko <aparhomenko@devexperts.com>
2018-01-16 15:18:00 +0300

    add watch tasks and lerna starter

--- packages/tools/src/scripts/watch-ts-transform.ts
+++ packages/tools/src/scripts/watch-ts-transform.ts
@@ -1,35 +1,8 @@
-import {SRC_PATH, DIST_PATH} from '../config/env';
-import * as spawn from 'cross-spawn';
-
 import {patchConsole} from '../utils/patchConsole';
 
 patchConsole('watch-ts-transform');
 
 console.log('started');
+process.argv = ['-w'];
 
-const result = spawn.sync(
-	require.resolve('typescript/bin/tsc'),
-	['-w'],
-	{
-		stdio: 'inherit',
-		cwd: SRC_PATH
-	}
-);
-if (result.signal) {
-	if (result.signal === 'SIGKILL') {
-		console.log(
-			'The build failed because the process exited too early. ' +
-			'This probably means the system ran out of memory or someone called ' +
-			'`kill -9` on the process.'
-		);
-	} else if (result.signal === 'SIGTERM') {
-		console.log(
-			'The build failed because the process exited too early. ' +
-			'Someone might have called `kill` or `killall`, or the system could ' +
-			'be shutting down.'
-		);
-	}
-	process.exit(1);
-}
-
-process.exit(result.status);
+require('typescript/bin/tsc');
