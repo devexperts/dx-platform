eb7ed969cc110cd6e50a5b219899838d1971f739
Artem Parhomenko <aparhomenko@devexperts.com>
2018-02-07 17:17:39 -0800

    fix grid

--- packages/react-kit/src/components/Grid/Grid.tsx
+++ packages/react-kit/src/components/Grid/Grid.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import prefix from '@devexperts/utils/lib/dom/prefix';
