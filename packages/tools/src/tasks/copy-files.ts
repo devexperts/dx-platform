5e4b60df0ef023df4a97df77e1431f3b58231451
Artem Parhomenko <aparhomenko@devexperts.com>
2018-03-06 15:29:00 +0300

    v0.9.2-0

--- packages/tools/src/tasks/copy-files.ts
+++
@@ -1,8 +0,0 @@
-import * as gulp from 'gulp';
-
-export const registerCopyFilesTask = (src: string, dist: string) => {
-	return gulp.task("copy-files", function () {
-		return gulp.src(`${src}/**/!(*.ts|*.tsx|*.icon.svg)`)
-			.pipe(gulp.dest(dist));
-	});
-};
