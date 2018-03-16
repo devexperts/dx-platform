38fa9b9d033f934b1b66b24c12750331f1551d84
Artem Parhomenko <aparhomenko@devexperts.com>
2018-03-16 15:15:12 +0300

    v0.9.3-2

---
+++ packages/tools/src/config/build/bundle.ts
@@ -0,0 +1,93 @@
+import { SRC_PATH, ROOT } from '../env';
+import { Configuration } from 'webpack';
+import {
+	TS_PATTERN,
+	STYLUS_PATTERN,
+	CSS_PATTERN,
+	FILE_LOADER_EXCLUDES,
+	tsLoader,
+	babelLoader,
+	styleLoader,
+	stylusLoader,
+	cssLoader,
+	postcssLoader,
+	fileLoader
+} from '../webpack/loaders';
+
+import {
+	createForkTSCheckerPlugin,
+	createHtmlPlugin,
+	createCssExtractTextPlugin
+} from '../webpack/plugins';
+
+const cssExtractor = createCssExtractTextPlugin();
+import * as ENV from '../env';
+
+const prodConfig: Configuration = {
+	resolve: {
+		symlinks: true,
+		extensions: ['.ts', '.tsx', '.js', '.jsx', '.styl'],
+	},
+	resolveLoader: {
+		modules: [ENV.TOOLS_NODE_MODULES_PATH]
+	},
+	entry: {
+		index: [
+			require.resolve(`${SRC_PATH}/index.tsx`),
+		]
+	},
+	output: {
+		path: `${ROOT}/build`,
+		filename: '[name].[chunkhash:8].js'
+	},
+	module: {
+		rules: [
+			{
+				oneOf: [
+					{
+						test: TS_PATTERN,
+						use: [
+							babelLoader,
+							tsLoader,
+						],
+					},
+					{
+						test: STYLUS_PATTERN,
+						use: cssExtractor.extract({
+							fallback: styleLoader,
+							use: [
+								cssLoader,
+								postcssLoader,
+								stylusLoader,
+							]
+						})
+					},
+					{
+						test: CSS_PATTERN,
+						use: cssExtractor.extract({
+							fallback: styleLoader,
+							use: [
+								cssLoader,
+								postcssLoader,
+							]
+						})
+					},
+					{
+						exclude: FILE_LOADER_EXCLUDES,
+						use: [
+							fileLoader,
+						],
+					} as any, // typings for webpack doesn't support default case properly
+				],
+			},
+		],
+	},
+	plugins: [
+		cssExtractor,
+		createHtmlPlugin(),
+		createForkTSCheckerPlugin(),
+	],
+};
+
+export {prodConfig as default};
+
---
+++ packages/tools/src/scripts/build-bundle.ts
@@ -0,0 +1,17 @@
+import { getProgramForScript } from '../utils/program';
+import { Scripts } from './constants';
+
+const defaultConfig = require.resolve('../config/build/prod');
+
+const program = getProgramForScript(Scripts.BUILD);
+
+program
+	.command('build')
+	.option('-c, --config [config]', 'path to webpack config', defaultConfig)
+	.action(function(options) {
+		const [node, bin] = program.rawArgs;
+		process.argv = [node, bin, '--config', options.config];
+		require('webpack/bin/webpack.js');
+	});
+
+program.parse(process.argv);
