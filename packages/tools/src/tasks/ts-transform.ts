5653540e546b3a6c1a70f67969cff3ae45bd6300
Anton Sutarmin <asutarmin@devexperts.com>
2018-04-19 10:39:08 +0300

    exit code when tsc fails

--- packages/tools/src/tasks/ts-transform.ts
+++ packages/tools/src/tasks/ts-transform.ts
@@ -1,6 +1,12 @@
 import { spawnStreaming } from '../utils/child-process.utils';
 import { ExecaChildProcess } from 'execa';
 
+function onTscExit(code: number, signal: string) {
+	if (code > 0) {
+		process.exitCode = 1;
+	}
+}
+
 export const startTransform: (src: string, dist: string, tsconfig: string, isWatch: boolean) => ExecaChildProcess =
 	(src, dist, tsconfig, isWatch) => {
 
@@ -12,5 +18,6 @@ export const startTransform: (src: string, dist: string, tsconfig: string, isWat
 		args.push('--diagnostics')
 	}
 
-	return spawnStreaming(`${require.resolve('typescript/bin/tsc')}`, 'tsc', args);
+	return spawnStreaming(`${require.resolve('typescript/bin/tsc')}`, 'tsc', args)
+			.on('exit', onTscExit);
 };
