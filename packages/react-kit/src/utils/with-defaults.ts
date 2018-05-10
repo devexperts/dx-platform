920c929d870c98cf208a2dee4327121625adf218
scink <a.v.koshkin.91@gmail.com>
2018-05-10 13:57:41 +0300

    adds with-rx and with-defaults

---
+++ packages/react-kit/src/utils/with-defaults.ts
@@ -0,0 +1,9 @@
+import {ComponentType} from 'react';
+import {PartialKeys} from '@devexperts/utils/dist/object/object';
+
+export const withDefaults = <P extends object, Keys extends keyof P>(
+	defaults: Pick<P, Keys>,
+): ((Target: ComponentType<P>) => ComponentType<PartialKeys<P, Keys>>) => Target => {
+	Target.defaultProps = Object.assign({}, Target.defaultProps || {}, defaults); // tslint:disable-line
+	return Target as any; // tslint:disable-line
+};
