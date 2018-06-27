74c5792de55df78f8ee617f7cdad2d282628314d
Artem Parhomenko <aparhomenko@devexperts.com>
2018-06-27 15:00:15 +0300

    introduce fail on error for 0.9.x tools

--- packages/tools/src/tasks/ts-transform.ts
+++ packages/tools/src/tasks/ts-transform.ts
@@ -1,16 +1,29 @@
 import { spawnStreaming } from '../utils/child-process.utils';
 import { ExecaChildProcess } from 'execa';
 
-export const startTransform: (src: string, dist: string, tsconfig: string, isWatch: boolean) => ExecaChildProcess =
-	(src, dist, tsconfig, isWatch) => {
+const onTscExit = (shouldFailOnError: boolean) => (code: number, signal: string) => {
+	if (code > 0 && shouldFailOnError) {
+		process.exitCode = 1;
+	}
+};
 
+export const startTransform = (
+	src: string,
+	dist: string,
+	tsconfig: string,
+	isWatch: boolean,
+	shouldFailOnError: boolean,
+): ExecaChildProcess => {
 	const args = ['-p', tsconfig, '--outDir', dist];
 
 	if (isWatch) {
 		args.push('-w');
 	} else {
-		args.push('--diagnostics')
+		args.push('--diagnostics');
 	}
 
-	return spawnStreaming(`${require.resolve('typescript/bin/tsc')}`, 'tsc', args);
+	return spawnStreaming(`${require.resolve('typescript/bin/tsc')}`, 'tsc', args).on(
+		'exit',
+		onTscExit(shouldFailOnError),
+	);
 };
