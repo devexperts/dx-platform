9770ba476cef132e3d845daba7a43ebe38223aa5
Anton Koshkin <akoshkin@devexperts.com>
2018-08-10 15:26:45 +0300

    temp

--- packages/react-kit/src/components/render-remote-data/demo/render-remote-data.page.tsx
+++ packages/react-kit/src/components/render-remote-data/demo/render-remote-data.page.tsx
@@ -2,7 +2,7 @@ import { storiesOf } from '@storybook/react';
 import { getRenderRemoteData } from '../get-render-remote-data';
 import * as React from 'react';
 import { SFC } from 'react';
-import { success } from '@devexperts/remote-data-ts';
+import { failure, success } from '@devexperts/remote-data-ts';
 
 const DataStateError: SFC<{ error: Error }> = props => <div>error: {props.error.message}</div>;
 const DataStateNoDate = () => <div>no data</div>;
@@ -17,8 +17,6 @@ const renderSuccess = (data: string) => <div>{data}</div>;
 storiesOf('RenderRemoteData', module)
 	.add('success', () => <RenderRemoteData success={renderSuccess} data={success('success')} />)
 	.add('error', () => (
-		<RenderRemoteData success={renderSuccess} data={DataStateError(new Error('some test error'))} />
+		<RenderRemoteData success={renderSuccess} data={failure<Error, string>(new Error('some test error'))} />
 	))
-	.add('no data', () => (
-		<RenderRemoteData success={renderSuccess} data={success('')} DataStateNoDate={data => !!data} />
-	));
+	.add('no data', () => <RenderRemoteData success={renderSuccess} data={success('')} noData={data => !!data} />);
