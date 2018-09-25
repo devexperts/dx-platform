9bacda10f2b427895e47972cd67590be180b0170
Kirill Agalakov <agalakov@devexperts.com>
2018-09-25 13:26:03 +0300

    feat(utils): add defer to Reader

--- packages/utils/src/adt/__tests__/reader.utils.test.ts
+++ packages/utils/src/adt/__tests__/reader.utils.test.ts
@@ -1,5 +1,5 @@
 import { asks } from 'fp-ts/lib/Reader';
-import { combine } from '../reader.utils';
+import { combine, defer } from '../reader.utils';
 
 type TFirst = {
 	foo: number;
@@ -10,36 +10,52 @@ type TSecond = {
 };
 
 describe('Reader utils', () => {
-	it('should combine context of provided readers', () => {
-		const first = asks((e: TFirst) => () => {});
-		const second = asks((e: TSecond) => () => {});
+	describe('combine', () => {
+		it('should combine context of provided readers', () => {
+			const first = asks((e: TFirst) => () => {});
+			const second = asks((e: TSecond) => () => {});
 
-		const combined = combine(first, second, (a, b, e) => {
-			expect(e.bar).toBe('bar');
-			expect(e.foo).toBe(2);
+			const combined = combine(first, second, (a, b, e) => {
+				expect(e.bar).toBe('bar');
+				expect(e.foo).toBe(2);
+			});
+
+			combined.run({
+				foo: 2,
+				bar: 'bar',
+			});
 		});
 
-		combined.run({
-			foo: 2,
-			bar: 'bar',
+		it('should be possible to use each components with bundled context', () => {
+			const fn = jest.fn();
+			const first = asks((e: TFirst) => () => fn(e.foo));
+			const second = asks((e: TSecond) => () => fn(e.bar));
+
+			const combined = combine(first, second, (a, b, e) => {
+				a();
+				expect(fn).toHaveBeenCalledWith(2);
+				b();
+				expect(fn).toHaveBeenCalledWith('bar');
+			});
+
+			combined.run({
+				foo: 2,
+				bar: 'bar',
+			});
 		});
 	});
 
-	it('should be possible to use each components with bundled context', () => {
-		const fn = jest.fn();
-		const first = asks((e: TFirst) => () => fn(e.foo));
-		const second = asks((e: TSecond) => () => fn(e.bar));
-
-		const combined = combine(first, second, (a, b, e) => {
-			a();
-			expect(fn).toHaveBeenCalledWith(2);
-			b();
-			expect(fn).toHaveBeenCalledWith('bar');
-		});
-
-		combined.run({
-			foo: 2,
-			bar: 'bar',
+	describe('defer', () => {
+		it('should defer part of the context', () => {
+			type E = TFirst & TSecond;
+			const result = defer(asks((e: E) => 0), 'foo');
+			const withBar = result.run({
+				bar: '123',
+			});
+			const resolved = withBar.run({
+				foo: 123,
+			});
+			expect(resolved).toBe(0);
 		});
 	});
 });
