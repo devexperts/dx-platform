2e5de60c98a9bcd733b057173b07473a0ed3cc0b
green176 <greenmail92@gmail.com>
2019-04-09 20:54:03 +0700

    fixed code style

--- packages/react-kit/src/components/DateInput/DateInput.tsx
+++ packages/react-kit/src/components/DateInput/DateInput.tsx
@@ -435,7 +435,7 @@ class RawDateInput extends React.Component<TDateInputFullProps, TDateInputState>
 	onSteppableInputClick = (e: React.MouseEvent<HTMLElement>) => {
 		if (this.props.isDisabled) {
 			return;
-        }
+		}
 
 		if (this.state.isOpened) {
 			const calendarButtonDOMNode = ReactDOM.findDOMNode(this.calendarButtonRef);
