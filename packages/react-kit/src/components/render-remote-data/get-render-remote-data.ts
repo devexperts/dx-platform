5a1f6a3025b5d735b325a270ab6786270e1d5ae1
Anton Koshkin <akoshkin@devexperts.com>
2019-04-28 02:38:13 +0300

    feat(react-kit): add ability to customize data states

--- packages/react-kit/src/components/render-remote-data/get-render-remote-data.ts
+++ packages/react-kit/src/components/render-remote-data/get-render-remote-data.ts
@@ -7,7 +7,7 @@ import {
 import { ComponentType, createElement } from 'react';
 
 export const getRenderRemoteData = <L>(dataStates: TRenderRemoteDataStates<L>) => <A>(
-	props: TRenderRemoteDataMainProps<L, A>,
+	props: TRenderRemoteDataMainProps<L, A> & Partial<TRenderRemoteDataStates<L>>,
 ) => {
 	const rrdProps: TRenderRemoteDataProps<L, A> = { ...dataStates, ...props };
 	const Target: ComponentType<TRenderRemoteDataProps<L, A>> = RenderRemoteData;
