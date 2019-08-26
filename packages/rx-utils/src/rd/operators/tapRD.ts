f2f7800cf1a6359c4dbaacb34a6b7b7ba01fcce5
raveclassic <raveclassic@gmail.com>
2019-08-26 14:04:17 +0100

    refactor(rx-utils): fp-ts@^2.0.5

--- packages/rx-utils/src/rd/operators/tapRD.ts
+++ packages/rx-utils/src/rd/operators/tapRD.ts
@@ -1,14 +1,6 @@
-import { Observable } from 'rxjs';
-import { RemoteData } from '@devexperts/remote-data-ts';
+import { MonoTypeOperatorFunction } from 'rxjs';
+import { isSuccess, RemoteData } from '@devexperts/remote-data-ts';
 import { tap } from 'rxjs/operators';
-import { Function1 } from 'fp-ts/lib/function';
 
-export function tapRD<L, A>(f: Function1<A, void>) {
-	return function tapRDOperatorFunction(source: Observable<RemoteData<L, A>>): Observable<RemoteData<L, A>> {
-		return tap((data: RemoteData<L, A>) => {
-			if (data.isSuccess()) {
-				f(data.value);
-			}
-		})(source);
-	};
-}
+export const tapRD = <L, A>(f: (a: A) => void): MonoTypeOperatorFunction<RemoteData<L, A>> =>
+	tap(data => isSuccess(data) && f(data.value));
