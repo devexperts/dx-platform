1dbd6df2def88c2ff5a0f566a0e2539b90355a1b
raveclassic <raveclassic@gmail.com>
2019-08-26 14:46:28 +0100

    refactor(react-kit): fp-ts@^2.0.5
    Updated DateInput

--- packages/react-kit/src/components/DateInput/DateInput.model.ts
+++ packages/react-kit/src/components/DateInput/DateInput.model.ts
@@ -1,6 +1,7 @@
-import { Option, getSetoid, some, option } from 'fp-ts/lib/Option';
-import { setoidNumber, getRecordSetoid } from 'fp-ts/lib/Setoid';
-import { liftA3 } from 'fp-ts/lib/Apply';
+import { Option, getEq, some, map, alt, fold, ap } from 'fp-ts/lib/Option';
+import { eqNumber, getStructEq } from 'fp-ts/lib/Eq';
+import { pipe } from 'fp-ts/lib/pipeable';
+import { constant } from 'fp-ts/lib/function';
 
 export type TDateInputValue = {
 	day: Option<number>;
@@ -8,17 +9,19 @@ export type TDateInputValue = {
 	year: Option<number>;
 };
 
-const setoidOptionNumber = getSetoid(setoidNumber);
-const dateSetoid = getRecordSetoid<TDateInputValue>({
-	day: setoidOptionNumber,
-	month: setoidOptionNumber,
-	year: setoidOptionNumber,
+const eqOptionNumber = getEq(eqNumber);
+const eqDateInputValue = getStructEq<TDateInputValue>({
+	day: eqOptionNumber,
+	month: eqOptionNumber,
+	year: eqOptionNumber,
 });
 
-export const isDatesDifferent = (x: TDateInputValue, y: TDateInputValue): boolean => !dateSetoid.equals(x, y);
+export const isDatesDifferent = (x: TDateInputValue, y: TDateInputValue): boolean => !eqDateInputValue.equals(x, y);
 
 export const buildDate = (day: number) => (month: number) => (year: number) => new Date(year, month, day);
-export const buildDateOption = liftA3(option)(buildDate);
+export const buildDateOption = (day: Option<number>) => (month: Option<number>) => (
+	year: Option<number>,
+): Option<Date> => pipe(day, map(buildDate), ap(month), ap(year));
 
 export enum DateFormatType {
 	MDY,
@@ -43,41 +46,44 @@ export const toObjectDate = (date: Date): TDateInputValue => ({
 });
 
 export function format(date: Option<number>, section: ActiveSection): string {
-	return date.foldL(
-		() => {
-			switch (section) {
-				case ActiveSection.Day: {
-					return 'dd';
-				}
-				case ActiveSection.Month: {
-					return 'mm';
-				}
-				case ActiveSection.Year: {
-					return 'yyyy';
-				}
-			}
-		},
-		value => {
-			switch (section) {
-				//maybe we should use left-pad here? ;)
-				case ActiveSection.Day: //fallthrough
-				case ActiveSection.Month: {
-					//day and month are 2 digits
-					return `${value >= 0 && value < 10 ? 0 : ''}${value}`;
-				}
-				case ActiveSection.Year: {
-					if (value < 10) {
-						return `000${value}`;
-					} else if (value < 100) {
-						return `00${value}`;
-					} else if (value < 1000) {
-						return `0${value}`;
-					} else {
-						return `${value}`;
+	return pipe(
+		date,
+		fold(
+			() => {
+				switch (section) {
+					case ActiveSection.Day: {
+						return 'dd';
+					}
+					case ActiveSection.Month: {
+						return 'mm';
+					}
+					case ActiveSection.Year: {
+						return 'yyyy';
 					}
 				}
-			}
-		},
+			},
+			value => {
+				switch (section) {
+					//maybe we should use left-pad here? ;)
+					case ActiveSection.Day: //fallthrough
+					case ActiveSection.Month: {
+						//day and month are 2 digits
+						return `${value >= 0 && value < 10 ? 0 : ''}${value}`;
+					}
+					case ActiveSection.Year: {
+						if (value < 10) {
+							return `000${value}`;
+						} else if (value < 100) {
+							return `00${value}`;
+						} else if (value < 1000) {
+							return `0${value}`;
+						} else {
+							return `${value}`;
+						}
+					}
+				}
+			},
+		),
 	);
 }
 
@@ -89,7 +95,7 @@ export function decrementMonth(month: number): number {
 }
 
 export function decrementMonthOption(month: Option<number>): Option<number> {
-	return month.map(decrementMonth).alt(some(11));
+	return pipe(month, map(decrementMonth), alt(constant(some(11))));
 }
 
 export function incrementMonth(month: number): number {
@@ -100,5 +106,7 @@ export function incrementMonth(month: number): number {
 }
 
 export function incrementMonthOption(month: Option<number>): Option<number> {
-	return month.map(incrementMonth).alt(some(0));
+	return pipe(month, map(incrementMonth), alt(constant(some(0))));
 }
+
+export const inc = (value: number): number => value + 1;
