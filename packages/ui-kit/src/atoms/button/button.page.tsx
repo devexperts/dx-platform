b3a5d0aaacf2b974523bc275cbbcb73e95df3dae
Kirill Agalakov <agalakov@devexperts.com>
2019-09-18 15:22:17 +0100

    feat(ui-kit): initial

---
+++ packages/ui-kit/src/atoms/button/button.page.tsx
@@ -0,0 +1,5 @@
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+import { Button } from './button';
+
+storiesOf('atom/button', module).add('Default', () => <Button />);
