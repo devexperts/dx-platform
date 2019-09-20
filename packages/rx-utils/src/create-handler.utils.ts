dca4b0388e65df2f8109abab9c9866e0f617cff7
Anton Koshkin <akoshkin@devexperts.com>
2019-09-20 11:07:32 +0300

    fix(rx-utils): createHandler void fix
    createHandler().handle now can be applied to the constant with type Lazy<void>
    fixes #177

--- packages/rx-utils/src/create-handler.utils.ts
+++ packages/rx-utils/src/create-handler.utils.ts
@@ -1,16 +1,15 @@
 import { Observable, Subject } from 'rxjs';
 
+export type THandle<A> = A extends void ? ((a?: void) => void) : ((a: A) => void);
 export type THandler<A> = {
-	readonly handle: (value: A) => void;
 	readonly value$: Observable<A>;
+	readonly handle: THandle<A>;
 };
 
 export const createHandler = <A = void>(): THandler<A> => {
 	const value = new Subject<A>();
-	const handle = (val: A) => value.next(val);
-
 	return {
 		value$: value.asObservable(),
-		handle,
+		handle: value.next as THandle<A>,
 	};
 };
