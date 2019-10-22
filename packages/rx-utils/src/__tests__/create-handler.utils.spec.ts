5aa698902489d207a4c19c520a8294d5a72c5659
Anton Koshkin <akoshkin@devexperts.com>
2019-10-22 18:42:53 +0300

    fix(rx-utils): createHandler union types fix

--- packages/rx-utils/src/__tests__/create-handler.utils.spec.ts
+++ packages/rx-utils/src/__tests__/create-handler.utils.spec.ts
@@ -1,5 +1,8 @@
 import { createHandler } from '../create-handler.utils';
 import { Endomorphism, Lazy } from 'fp-ts/lib/function';
+import { constVoid } from 'fp-ts/es6/function';
+
+type Effect<A> = (a: A) => void;
 
 describe('create-handler.utils.ts', function() {
 	describe('createHandler', function() {
@@ -7,18 +10,33 @@ describe('create-handler.utils.ts', function() {
 			const a = createHandler();
 			const af1: Lazy<void> = a.handle; // adds possibility to use handle as Lazy<void>
 			const af2: Endomorphism<void> = a.handle; // keeps possibility to use as Endomorphism<void>
+			a.handle();
+			a.handle(undefined);
+			a.handle(constVoid());
 			af1(); // no argument is needed
 			af2(undefined); // still can pass undefined
 
 			const b = createHandler<void>(); // the same as above
+			b.handle();
+			b.handle(undefined);
+			b.handle(constVoid());
 			const bf1: Lazy<void> = b.handle;
 			const bf2: Endomorphism<void> = b.handle;
 			bf1();
 			bf2(undefined);
 
 			const c = createHandler<string>();
-			const cf3: (a: string) => void = c.handle; // that's correct
-			cf3(''); // correct
+			const cf3: Effect<string> = c.handle;
+			cf3('');
+
+			const d = createHandler<boolean>();
+			const df1: Effect<boolean> = d.handle;
+			df1(true);
+
+			const e = createHandler<'a' | 'b'>();
+			e.handle('a');
+			const ef1: Effect<'a' | 'b'> = e.handle;
+			ef1('b');
 		});
 	});
 });
