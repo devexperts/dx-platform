94013f6b34e954af258fc56c84ad8a06e7db713e
Kirill Agalakov <agalakov@devexperts.com>
2019-12-12 17:42:09 +0300

    style: run eslint on *.js files

--- packages/react-kit/typings/element-resize-detector/index.d.ts
+++ packages/react-kit/typings/element-resize-detector/index.d.ts
@@ -1,6 +1,6 @@
 declare module 'element-resize-detector' {
 	type TDetectoryFactoryOptions = {
-		strategy?: 'scroll'
+		strategy?: 'scroll';
 	};
 	type THandler = (element: Element) => any;
 	//tslint:disable max-line-length
@@ -8,20 +8,20 @@ declare module 'element-resize-detector' {
 		/**
 		 * Listens to the element for resize events and calls the listener function with the element as argument on resize events.
 		 */
-		listenTo: (element: Element, handler: THandler) => void,
+		listenTo: (element: Element, handler: THandler) => void;
 
 		/**
 		 * Removes the listener from the elemens.
 		 */
-		removeListener: (element: Element, handler: THandler) => void,
+		removeListener: (element: Element, handler: THandler) => void;
 		/**
 		 * Removes all listeners from the element, but does not completely remove the detector. Use this function if you may add listeners later and don't want the detector to have to initialize again.
 		 */
-		removeAllListeners: (element: Element) => void,
+		removeAllListeners: (element: Element) => void;
 		/**
 		 *
 		 */
-		uninstall: (element: Element) => void
+		uninstall: (element: Element) => void;
 	};
 	//tslint:enable max-line-length
 
