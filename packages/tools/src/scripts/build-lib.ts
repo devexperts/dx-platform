dbd875cab84e171ef01ffcc8f43bbf456b27e071
Kirill Agalakov <agalakov@devexperts.com>
2019-12-16 19:38:38 +0300

    feat(tools): support -p/--project arg to specify custom tsconfig.json

--- packages/tools/src/scripts/build-lib.ts
+++ packages/tools/src/scripts/build-lib.ts
@@ -12,10 +12,11 @@ program
 	.command('build-lib <src> <dist>')
 	.option('-w, --watch', 'start in watch mode')
 	.option('-f, --failOnError', 'start in watch mode')
+	.option('-p, --project <project>', 'relative path to tsconfig.json')
 	.action(function(src, dist, options) {
 		const SRC_PATH = path.join(ROOT, src);
 		const DIST_PATH = path.join(ROOT, dist);
-		const TSCONFIG_PATH = path.join(ROOT, 'tsconfig.json');
+		const TSCONFIG_PATH = path.join(ROOT, options.project);
 
 		startSync(SRC_PATH, DIST_PATH, options.watch);
 		startTransform(SRC_PATH, DIST_PATH, TSCONFIG_PATH, options.watch, options.failOnError);
